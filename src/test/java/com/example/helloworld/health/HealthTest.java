c572b940ae778c350d5e5e53b12d3b3d7702b1de
Liu Haibin <liu.haibin@gmail.com>
2020-02-29 16:14:40 +0800

    Add health check test

---
+++ src/test/java/com/example/helloworld/health/HealthTest.java
@@ -0,0 +1,34 @@
+package com.example.helloworld.health;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.core.Response;
+
+import com.example.helloworld.HelloWorldApplication;
+import com.example.helloworld.HelloWorldConfiguration;
+
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import io.dropwizard.testing.ResourceHelpers;
+import io.dropwizard.testing.junit.DropwizardAppRule;
+
+public class HealthTest {
+
+    @ClassRule
+    public static final DropwizardAppRule<HelloWorldConfiguration> RULE =
+            new DropwizardAppRule<>(HelloWorldApplication.class, ResourceHelpers.resourceFilePath("config.yml"));
+
+    @Test
+    public void loginHandlerRedirectsAfterPost() {
+        Client client = RULE.client();
+
+        Response response = client.target(
+                 String.format("http://localhost:%d/healthcheck", RULE.getAdminPort()))
+                .request()
+                .get();
+
+        assertThat(response.getStatus()).isEqualTo(200);
+    }
+}
