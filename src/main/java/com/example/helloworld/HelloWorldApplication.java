7b367aa96b672f92180311e088ab25240d34fa11
Liu Haibin <liu.haibin@gmail.com>
2020-02-29 22:55:03 +0800

    Add guice

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -1,10 +1,14 @@
 package com.example.helloworld;
 
+import com.hubspot.dropwizard.guice.GuiceBundle;
+
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
+
+    private GuiceBundle<HelloWorldConfiguration> guiceBundle;
 
     public static void main(final String[] args) throws Exception {
         new HelloWorldApplication().run(args);
@@ -17,13 +21,19 @@ public class HelloWorldApplication extends Application<HelloWorldConfiguration> 
 
     @Override
     public void initialize(final Bootstrap<HelloWorldConfiguration> bootstrap) {
-        // TODO: application initialization
+        guiceBundle = GuiceBundle.<HelloWorldConfiguration>newBuilder()
+            .addModule(new HelloWorldModule())
+            .enableAutoConfig(getClass().getPackage().getName())
+            .setConfigClass(HelloWorldConfiguration.class)
+            .build();
+  
+        bootstrap.addBundle(guiceBundle);
     }
 
     @Override
-    public void run(final HelloWorldConfiguration configuration,
-                    final Environment environment) {
-        // TODO: implement application
+    public void run(final HelloWorldConfiguration configuration, final Environment environment) {
+        // now you don't need to add resources, tasks, healthchecks or providers
+        // you must have your health checks inherit from InjectableHealthCheck in order for them to be injected
     }
 
 }
---
+++ src/main/java/com/example/helloworld/HelloWorldModule.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+import com.google.inject.AbstractModule;
+
+public class HelloWorldModule extends AbstractModule {
+    @Override
+    protected void configure() {
+      // anything you'd like to configure
+    }
+}
